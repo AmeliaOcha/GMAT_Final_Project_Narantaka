dafe54be1e77118e6016c259dd810c795a034f1e
AmeliaOcha <ameliaochamaharani1@gmil.com>
2024-10-25 22:05:49 +0700

    upgit reflog

---
+++ Final_img_proc.java
@@ -0,0 +1,26 @@
+//package com.example.javasp;
+
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.*;
+import org.opencv.features2d.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.highgui.HighGui;
+
+import java.lang.annotation.Native;
+import java.lang.classfile.components.CodeRelabeler;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Final_img_proc {
+    static {
+      System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+  }
+  // Memuat library native OpenCV yang diperlukan untuk menjalankan kode
+
+  public static void main(String[] args) {
+
+    //testing
+    System.out.println("Hello World");
+  }
+}
